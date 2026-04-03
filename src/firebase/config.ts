11efd2b59a31877b3d395a4afb49d19912a43c36
Ang Pangalan Mo <iyong-email@example.com>
2026-04-03 22:50:22 +0800

    fix: Update Firebase config to production

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 // Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDO4fPfojgEbBDk-dp2ndI0JFKVRP98mHM",
-  authDomain: "testing-3b2d7.firebaseapp.com",
-  projectId: "testing-3b2d7",
-  storageBucket: "testing-3b2d7.firebasestorage.app",
-  messagingSenderId: "330051157468",
-  appId: "1:330051157468:web:018e44f1a9a883bd84a319"
+  apiKey: "AIzaSyAodBTXJtAms7AiLTCV656OIC2my0JY5eA",
+  authDomain: "queuebyescr.firebaseapp.com",
+  projectId: "queuebyescr",
+  storageBucket: "queuebyescr.firebasestorage.app",
+  messagingSenderId: "641492955348",
+  appId: "1:641492955348:web:865472c6f0453da2974b41"
 };
