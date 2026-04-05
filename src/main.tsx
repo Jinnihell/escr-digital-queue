87249bc3627251813f22de6975a3cfb16a2df8fe
Ang Pangalan Mo <iyong-email@example.com>
2026-04-06 00:25:21 +0800

    Fix duplicate AlertProvider and improve overall system

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AlertProvider } from './context/AlertContext'
-import Alert from './components/Alert'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AlertProvider>
-      <Alert />
-      <App />
-    </AlertProvider>
+    <App />
   </StrictMode>,
 )
